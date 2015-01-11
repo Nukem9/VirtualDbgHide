b7f2ccc837732ea99af4b645e569a0f1f1887796
Nukem <Nukem@outlook.com>
2015-01-10 23:44:57 -0500

    More I/O

--- VirtualDbgHide/IoControl.c
+++ VirtualDbgHide/IoControl.c
@@ -2,6 +2,8 @@
 
 #define VM_DETOUR_SYSCALLS		CTL_CODE(FILE_DEVICE_UNKNOWN, 0x900, METHOD_BUFFERED, FILE_ANY_ACCESS)
 #define VM_BEGIN_VIRTUALIZATION CTL_CODE(FILE_DEVICE_UNKNOWN, 0x901, METHOD_BUFFERED, FILE_ANY_ACCESS)
+#define VM_STOP_VIRTUALIZATION	CTL_CODE(FILE_DEVICE_UNKNOWN, 0x902, METHOD_BUFFERED, FILE_ANY_ACCESS)
+#define VM_QUERY_RUNNING		CTL_CODE(FILE_DEVICE_UNKNOWN, 0x903, METHOD_BUFFERED, FILE_ANY_ACCESS)
 
 NTSTATUS DispatchIoControl(PDEVICE_OBJECT DeviceObject, PIRP Irp)
 {
@@ -15,15 +17,62 @@ NTSTATUS DispatchIoControl(PDEVICE_OBJECT DeviceObject, PIRP Irp)
 	//
 	if (ioStack->MajorFunction == IRP_MJ_DEVICE_CONTROL)
 	{
-		ULONG controlCode = ioStack->Parameters.DeviceIoControl.IoControlCode;
+		ULONG controlCode	= ioStack->Parameters.DeviceIoControl.IoControlCode;
+		ULONG inputLength	= ioStack->Parameters.DeviceIoControl.InputBufferLength;
+		ULONG outputLength	= ioStack->Parameters.DeviceIoControl.OutputBufferLength;
 
 		switch (controlCode)
 		{
 		case VM_DETOUR_SYSCALLS:
-			break;
+		{
+			//
+			// Information length check
+			//
+			if (inputLength != sizeof(SYSCALL_HOOK_INFO))
+			{
+				status = STATUS_INFO_LENGTH_MISMATCH;
+				break;
+			}
+
+			PSYSCALL_HOOK_INFO info = Irp->AssociatedIrp.SystemBuffer;
+		}
+		break;
 
 		case VM_BEGIN_VIRTUALIZATION:
-			break;
+		{
+			//
+			// Start the virtual machine initialization thread
+			//
+			HANDLE threadHandle;
+			status = PsCreateSystemThread(&threadHandle, THREAD_ALL_ACCESS, NULL, NULL, NULL, VmStart, NULL);
+
+			if (NT_SUCCESS(status))
+				NtClose(threadHandle);
+		}
+		break;
+
+		case VM_STOP_VIRTUALIZATION:
+		{
+			status = STATUS_NOT_IMPLEMENTED;
+		}
+		break;
+
+		case VM_QUERY_RUNNING:
+		{
+			//
+			// Output buffer size is expected to be a CHAR
+			//
+			if (outputLength != sizeof(CHAR))
+			{
+				status = STATUS_INFO_LENGTH_MISMATCH;
+				break;
+			}
+
+			*(CHAR *)Irp->AssociatedIrp.SystemBuffer	= VmIsActive();
+			Irp->IoStatus.Information					= sizeof(CHAR);
+		}
+		break;
+
 		}
 	}
 
